50afd4b4c3a2f2c38d5adaa516a5dd3511ddce40
Francisco <franciscoocontatoo@gmail.com>
2025-11-02 00:38:13 -0300

    RabbitMQConsumer: Criando o serviços para consumir os dados que são enviados através do RabbitMQ

--- src/AutomotiveStock.CentralStock/Program.cs
+++ src/AutomotiveStock.CentralStock/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutomotiveStock.Shared.Contracts;
+using AutomotiveStock.Shared.Services;
+using System.Text.Json;
+
+Console.WriteLine("--- Sistema Central de Estoque (Consumer) ---");
+Console.WriteLine("Aguardando eventos...");
+Console.WriteLine("----------------------------------------------");
+
+// 1. Instanciamos nosso serviços
+var rabbitMQServices = new RabbitMQServices("localhost");
+
+// 2. Defininmos a função de callback
+// Obs.: Lógica executada a cada mensagem recebida
+Action<string> handleConsumptionEvent = (message) =>
+{
+    try
+    {
+        // 2.1. Desserializamos a string json de volta para o nosso objeto em C#
+        var consumptionEvent = JsonSerializer.Deserialize<MaterialConsumptionEvent>(message);
+
+        if (consumptionEvent != null)
+        {
+            // 2.1.1. [MVP] Apenas imprimimos os dados
+            // Obs.: No futuro, aqui é onde chamaremos para atualizar o estoque
+            Console.WriteLine($"[+] Evento recebido (Planta: {consumptionEvent.PlantConsumption})");
+            Console.WriteLine($"Material: {consumptionEvent.MaterialCode}");
+            Console.WriteLine($"Quantidade: {consumptionEvent.QtyConsumed}");
+            Console.WriteLine($"Ordem: {consumptionEvent.ProductionOrder}");
+            Console.WriteLine($"Motivo: {consumptionEvent.ConsumedReason}");
+            Console.WriteLine("------------------------------------------------------------------");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[!] Erro ao desserializar/processar: {ex.Message}");
+    }
+};
+
+// 3. Definimos os parâmetros da seção de documentos
+string queueName = "queue.central.stock";
+string routingKey = "consumption.*";
+
+// 4. Iniciamos o "Ouvinte"
+rabbitMQServices.StartConsuming(queueName, routingKey, handleConsumptionEvent);
+
+// 5. Mantemos o Console app rodando
+Console.WriteLine("Pressione [Enter] para sair.");
+Console.ReadLine();
+
+// 6. Limpamos a conexão ao sair
+rabbitMQServices.DisposeConsumer();
+Console.WriteLine("Encerrando consumidor");
--- src/AutomotiveStock.Shared/Services/RabbitMQServices.cs
+++ src/AutomotiveStock.Shared/Services/RabbitMQServices.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using RabbitMQ.Client;
 using System.Text.Json;
 using System.Text;
+using RabbitMQ.Client.Events;
 
 namespace AutomotiveStock.Shared.Services
 {
@@ -14,6 +15,9 @@ namespace AutomotiveStock.Shared.Services
         private readonly ConnectionFactory _factory;
         private readonly string _exchangeName = "stock.events";
 
+        private IConnection _consumerConnection;
+        private IModel _consumerChannel;
+
         public RabbitMQServices(string hostname)
         {
             _factory = new ConnectionFactory()
@@ -21,7 +25,7 @@ namespace AutomotiveStock.Shared.Services
                 HostName = hostname
             };
         }
-        
+
         public void PublishEvent(string routingKey, object payload)
         {
             using (var connection = _factory.CreateConnection())
@@ -44,6 +48,56 @@ namespace AutomotiveStock.Shared.Services
                 Console.WriteLine($"[x] Event Publish: '{routingKey}'");
                 Console.WriteLine($"Message: {message}");
             }
+        }
+
+        public void StartConsuming(string queueName, string routingKey, Action<string> onMessageReceived)
+        {
+            // 1. Criamos um canal de conexão e canal de longa duração
+            _consumerConnection = _factory.CreateConnection();
+            _consumerChannel = _consumerConnection.CreateModel();
+
+            // 2. Declaramos a Exchange (para garantir que existe)
+            _consumerChannel.ExchangeDeclare(exchange: _exchangeName, type: ExchangeType.Topic, durable: true);
+
+            // 3. Declaramos a fila 
+            _consumerChannel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            // 4. Fazemos o "Binding" da fila na Exchange com o Routing Key
+            _consumerChannel.QueueBind(queue: queueName, exchange: _exchangeName, routingKey: routingKey);
+
+            // 5. Criamos o consumidor
+            var consumer = new EventingBasicConsumer(_consumerChannel);
+
+            // 6. Implementamos o evento "Received" (O que fazer quando a msg chegar)
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    // 6.1. Pegamos a mensagem e convertemos de Byte para string
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    // 6.2. Chamamos a função de callback que o program.cs nos passou
+                    onMessageReceived(message);
+
+                    // 6.3. Avisamos ao RabbitMQ: "Ok, recebi e processei a msg. Podemos apagar da fila"
+                    _consumerChannel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[!] Erro ao processar mensagem: {ex.Message}");
+                }
+            };
+
+            // 7. Inciamos o consumo
+            _consumerChannel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+        }
+        
+        // 8. Fecha a conexão e o canal do consumidor
+        public void DisposeConsumer()
+        {
+            _consumerChannel?.Dispose();
+            _consumerConnection?.Dispose();
         }
     }
 }
