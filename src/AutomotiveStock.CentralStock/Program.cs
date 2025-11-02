2f929bda7fa6e2bf5f20c246aefdf14bf87483ed
Francisco <franciscoocontatoo@gmail.com>
2025-11-02 01:19:21 -0300

    LogsApplication: Criando uma lógica para geração de logs em tempo de execução da aplicação

--- src/AutomotiveStock.CentralStock/Program.cs
+++ src/AutomotiveStock.CentralStock/Program.cs
@@ -1,52 +1,77 @@
 ﻿using AutomotiveStock.Shared.Contracts;
 using AutomotiveStock.Shared.Services;
 using System.Text.Json;
+using Serilog;
 
-Console.WriteLine("--- Sistema Central de Estoque (Consumer) ---");
-Console.WriteLine("Aguardando eventos...");
-Console.WriteLine("----------------------------------------------");
+// Funcionalidade para geração de Logs
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.File("logs/central_stock_.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .CreateLogger();
 
-// 1. Instanciamos nosso serviços
-var rabbitMQServices = new RabbitMQServices("localhost");
-
-// 2. Defininmos a função de callback
-// Obs.: Lógica executada a cada mensagem recebida
-Action<string> handleConsumptionEvent = (message) =>
+try
 {
-    try
-    {
-        // 2.1. Desserializamos a string json de volta para o nosso objeto em C#
-        var consumptionEvent = JsonSerializer.Deserialize<MaterialConsumptionEvent>(message);
+    Log.Information("--- Sistema Central de Estoque (Consumer) ---");
+    Log.Information("Aguardando eventos...");
+    Log.Information("----------------------------------------------");
 
-        if (consumptionEvent != null)
+    // 1. Instanciamos nosso serviços
+    var rabbitMQServices = new RabbitMQServices("localhost");
+
+    // 2. Defininmos a função de callback
+    // Obs.: Lógica executada a cada mensagem recebida
+    Action<string> handleConsumptionEvent = (message) =>
+    {
+        try
         {
-            // 2.1.1. [MVP] Apenas imprimimos os dados
-            // Obs.: No futuro, aqui é onde chamaremos para atualizar o estoque
-            Console.WriteLine($"[+] Evento recebido (Planta: {consumptionEvent.PlantConsumption})");
-            Console.WriteLine($"Material: {consumptionEvent.MaterialCode}");
-            Console.WriteLine($"Quantidade: {consumptionEvent.QtyConsumed}");
-            Console.WriteLine($"Ordem: {consumptionEvent.ProductionOrder}");
-            Console.WriteLine($"Motivo: {consumptionEvent.ConsumedReason}");
-            Console.WriteLine("------------------------------------------------------------------");
+            // 2.1. Desserializamos a string json de volta para o nosso objeto em C#
+            var consumptionEvent = JsonSerializer.Deserialize<MaterialConsumptionEvent>(message);
+
+            if (consumptionEvent != null)
+            {
+                // 2.1.1. [MVP] Apenas imprimimos os dados
+                // Obs.: No futuro, aqui é onde chamaremos para atualizar o estoque
+                Log.Information(
+                    "Evento Recebido: Planta {Plant} | Material {MaterialCode} | Qty {Quantity} | Ordem {Order}",
+                    consumptionEvent.PlantConsumption,
+                    consumptionEvent.MaterialCode,
+                    consumptionEvent.QtyConsumed,
+                    consumptionEvent.ProductionOrder
+                );
+            }
         }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"[!] Erro ao desserializar/processar: {ex.Message}");
-    }
-};
+        catch (JsonException jsonEx)
+        {
+            Log.Error(jsonEx, "Falha ao desserializar a mensagem: {MessageBody}", message);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Erro desconhecido ao processar evento.");
+        }
+    };
 
-// 3. Definimos os parâmetros da seção de documentos
-string queueName = "queue.central.stock";
-string routingKey = "consumption.*";
+    // 3. Definimos os parâmetros da seção de documentos
+    string queueName = "queue.central.stock";
+    string routingKey = "consumption.*";
 
-// 4. Iniciamos o "Ouvinte"
-rabbitMQServices.StartConsuming(queueName, routingKey, handleConsumptionEvent);
+    // 4. Iniciamos o "Ouvinte"
+    rabbitMQServices.StartConsuming(queueName, routingKey, handleConsumptionEvent);
 
-// 5. Mantemos o Console app rodando
-Console.WriteLine("Pressione [Enter] para sair.");
-Console.ReadLine();
+    // 5. Mantemos o Console app rodando
+    Console.WriteLine("Pressione [Enter] para sair.");
+    Console.ReadLine();
 
-// 6. Limpamos a conexão ao sair
-rabbitMQServices.DisposeConsumer();
-Console.WriteLine("Encerrando consumidor");
+    // 6. Limpamos a conexão ao sair
+    rabbitMQServices.DisposeConsumer();
+    Console.WriteLine("Encerrando consumidor");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Aplicação do Sistema Central Falhou inesperadamente.");
+} finally
+{
+    Log.Information("--- Sistema Central de Estoque (Consumer) ENCERRADO ---");
+    Log.CloseAndFlush();
+}
--- src/AutomotiveStock.PlantSimulator/Program.cs
+++ src/AutomotiveStock.PlantSimulator/Program.cs
@@ -2,43 +2,69 @@
 using AutomotiveStock.Shared.Enums;
 using AutomotiveStock.Shared.Services;
 using System.Threading;
+using Serilog;
 
-Console.WriteLine("-- Simulador da Planta Automotiva (Publisher)");
 
-// 1. Connectamos ao RabbitMQ 
-var rabbitMqServices = new RabbitMQServices("localhost");
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.File("logs/plant_simulator_.txt", 
+        rollingInterval: RollingInterval.Day, 
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .CreateLogger();
 
-// 2. Criamos um loop para simular o consumo
-int eventCount = 1;
-while (true)
+try
 {
-    Console.WriteLine("\n------------------------------------------");
-    Console.WriteLine($"Simulando envio de eventos #{eventCount}...");
+    Log.Information("--- Simulador da Planta Automotiva (Publisher) INICIADO ---");
 
-    // 3. Criamos um evento de consumo falso, para teste
-    var consumptionEvent = new MaterialConsumptionEvent
-    {
-        DateTimeConsumption = DateTime.UtcNow,
-        PlantConsumption = "Planta A",
-        MaterialCode = "ACO-1020",
-        QtyConsumed = 150.5,
-        ProductionOrder = "OP-2025-001",
-        ConsumedReason = ConsumedReasons.Production
-    };
+    // 1. Connectamos ao RabbitMQ 
+    var rabbitMqServices = new RabbitMQServices("localhost");
 
-    // 4. Definimos a rota
-    var routingKey = "consumption.planta-a";
+    // 2. Criamos um loop para simular o consumo
+    int eventCount = 1;
+    while (true)
+    {
+        Log.Information("\n------------------------------------------");
+        Log.Information("Simulando envio do evento #{EventCount}...", eventCount);
 
-    // 5. Publicamos
-    try
-    {
-        rabbitMqServices.PublishEvent(routingKey, consumptionEvent);
-    } catch (Exception ex)
-    {
-        Console.WriteLine($" [!] Falha ao conectar/publicar: {ex.Message}");
-        Console.WriteLine("    Verifique se o RabbitMQ está em execução.");
+        // 3. Criamos um evento de consumo falso, para teste
+        var consumptionEvent = new MaterialConsumptionEvent
+        {
+            DateTimeConsumption = DateTime.UtcNow,
+            PlantConsumption = "Planta A",
+            MaterialCode = "ACO-1020",
+            QtyConsumed = 150.5,
+            ProductionOrder = "OP-2025-001",
+            ConsumedReason = ConsumedReasons.Production
+        };
+
+        // 4. Definimos a rota
+        var routingKey = "consumption.planta-a";
+
+        // 5. Publicamos
+        try
+        {
+            rabbitMqServices.PublishEvent(routingKey, consumptionEvent);
+
+            Log.Information("Evento Publicado: {RoutingKey} | EventId: {EventId}", routingKey, consumptionEvent.EventId);
+            eventCount++;
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "[!] Falha ao conectar/publicar");
+        }
+
+        // Aguarda 5 segundos para o próximo evento
+        Thread.Sleep(5000);
     }
-
-    // Aguarda 5 segundos para o próximo evento
-    Thread.Sleep(5000);
+}catch (Exception ex)
+{
+    Log.Fatal(ex, "Aplicação do Simulador falhou inesperadamente.");
+}
+finally
+{
+    // 5. ESSENCIAL: Garantir que os logs sejam gravados antes de fechar
+    Log.Information("--- Simulador da Planta Automotiva (Publisher) ENCERRADO ---");
+    Log.CloseAndFlush();
 }
