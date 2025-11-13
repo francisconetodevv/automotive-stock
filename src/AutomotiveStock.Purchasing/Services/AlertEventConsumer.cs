d34575d45316337bc9650afb1089f9a3f1b9ee42
Francisco <franciscoocontatoo@gmail.com>
2025-11-13 20:33:33 -0300

    feat: Trabalhando sobre o projeto de Compras

--- src/AutomotiveStock.Purchasing/Services/AlertEventConsumer.cs
+++ src/AutomotiveStock.Purchasing/Services/AlertEventConsumer.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using AutomotiveStock.Shared.Contracts;
 using AutomotiveStock.Shared.Services;
+using Serilog;
 
 namespace AutomotiveStock.Purchasing.Services
 {
@@ -16,7 +19,33 @@ namespace AutomotiveStock.Purchasing.Services
 
         public void StartListening()
         {
-            
+            Log.Information("Iniciando consumidor...");
+            Log.Information("----------------------------------------------");
+
+            string queue = "queue.purchasing.alerts";
+            string routingKey = "alert.lowstock"; // Verificar
+
+            _rabbitMQServices.StartConsuming(queue, routingKey, OnMessageReceived);
+        }
+
+        private void OnMessageReceived(string message)
+        {
+            try
+            {
+                var consumptionEvent = JsonSerializer.Deserialize<LowStockAlertEvent>(message);
+
+                if (consumptionEvent != null)
+                {
+                    Log.Information(
+                        "ORDEM DE COMPRA GERADA: Material {MaterialCode} | Estoque Atual: {CurrentStock} kg",
+                        consumptionEvent.MaterialCode,
+                        consumptionEvent.CurrentStock
+                    );
+                }
+            } catch (Exception ex)
+            {
+                Log.Error(ex, "Falha ao processar alerta de estoque baixo");
+            }
         }
     }
 }
