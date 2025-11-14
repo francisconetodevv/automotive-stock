6f002b4f1c31d2656038ce86fff172fc131891b2
Francisco <franciscoocontatoo@gmail.com>
2025-11-13 22:48:51 -0300

    feat: Trabalhando sobre o projeto de Compras

--- src/AutomotiveStock.Purchasing/Program.cs
+++ src/AutomotiveStock.Purchasing/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutomotiveStock.Purchasing.Services;
+using AutomotiveStock.Shared.Services;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .WriteTo.File("logs/purchasing.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+        .CreateLogger();
+
+RabbitMQServices rabbitMQServices = null;
+
+try
+{
+    Log.Information("--- Sistema Central de Compras INICIANDO ---");
+
+    rabbitMQServices = new RabbitMQServices("localhost");
+
+    var alertEventConsumer = new AlertEventConsumer(rabbitMQServices);
+    alertEventConsumer.StartListening();
+    Console.WriteLine("Pressione [Enter] para sair.");
+    Console.ReadLine();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Aplicação do Sistema de Compras Falhou inesperadamente.");
+}
+finally
+{
+    Log.Information("--- Sistema Central de Compras ENCERRANDO ---");
+    rabbitMQServices?.DisposeConsumer();
+    Log.CloseAndFlush();
+}
